57abf5e12aaecbf6a308640ddef4c89bb787a645
tsu-git <tsu.sugi@gmail.com>
2026-06-14 17:04:10 +0900

    華氏と摂氏の対応表出力プログラム

---
+++ fahr_and_celsius.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* fah=0.20, ..., 300に対して、摂氏-華氏対応表を印字する */
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* 温度表の下限 */
+    upper = 300;    /* 上限 */
+    step = 20;      /* 刻み */
+
+    /* ヘッダ出力 */
+    printf("fahr celsius\n");
+    printf("---- -------\n");
+
+    /* 対応表出力 */
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
